ad9225f109a55ee98ad1bbaa0b7123aab85f54fc
ArtofGaming <turner4965@gmail.com>
2021-04-03 23:38:28 -0700

    text p1

---
+++ Mall Mystery/Assets/Scripts/DialogueScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class DialogueScript : MonoBehaviour
+{
+    public TextAsset file;
+    Text dialogue;
+    char lineCutter = '\n';
+    char squareCutter = ',';
+    int x = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue.text = "Hi";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void readData ()
+    {
+        string[] lines = file.text.Split(lineCutter);
+        foreach(string line in lines)
+        {
+            string[] squares = file.text.Split(squareCutter);
+            foreach(string square in squares)
+            {
+                dialogue.text += squares[x] += "t";
+                x += 1;
+            }
+        }
+    }
+}
--- Mall Mystery/Assets/Scripts/Text.cs
+++ Mall Mystery/Assets/Scripts/Text.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
 
-public class NewBehaviourScript1 : MonoBehaviour
+
+public class Text : MonoBehaviour
 {
+    public TextAsset file;
+    int lineStart;
+    public Text texts;
+    char dialogueCutter = '\n';
+    char nameCutter = ',';
     // Start is called before the first frame update
     void Start()
     {
-        
+        readData(lineStart);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void readData(int lineNumber)
+    {
+        string[] lines = file.text.Split(dialogueCutter);
+        foreach (string line in lines)
+        {
+            string[] squares = file.text.Split(nameCutter);
+            foreach (string square in squares)
+            {
+                
+            }
+        }
     }
 }
