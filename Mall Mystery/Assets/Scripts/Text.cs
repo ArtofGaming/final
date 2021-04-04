0455205d876abd6b7c3da94e0da3688c687cadf5
ArtofGaming <turner4965@gmail.com>
2021-04-03 22:15:52 -0700

    try2

--- Mall Mystery/Assets/Scripts/Text.cs
+++ Mall Mystery/Assets/Scripts/Text.cs
@@ -2,35 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Text : MonoBehaviour
+public class NewBehaviourScript1 : MonoBehaviour
 {
-    public TextAsset csvFile;
-    private char lineSeperator = '\n';
-    private char fieldSeperator = ',';
-    public Text contentArea;
     // Start is called before the first frame update
     void Start()
     {
-        readLine();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void readLine()
-    {
-        string[] dialogue = csvFile.text.Split(lineSeperator);
-        foreach (string talk in dialogue)
-        {
-            string[] text = csvFile.text.Split(fieldSeperator);
-            Debug.Log(text);
-            foreach (string word in text)
-            {
-                contentArea.text += word + '\t';
-            }
-        }
     }
 }
